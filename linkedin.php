1f508a87c53f535332ae533374e512819e77e15f
Tavi <dragulceo@gmail.com>
2014-10-09 03:17:04 +0300

    Simple linkedin.php after refactoring

--- linkedin.php
+++ linkedin.php
@@ -5,42 +5,17 @@ require 'vendor/autoload.php';
 use Local\FileWriter;
 use Local\LinkedIn;
 
-// OAuth 2 Control Flow
-if (isset($_GET['error'])) {
-    // LinkedIn returned an error
-    print $_GET['error'] . ': ' . $_GET['error_description'];
-    exit;
-} elseif (isset($_GET['code'])) {
-    // User authorized your application
-    if ($_SESSION['state'] == $_GET['state']) {
-        // Get token so you can make API calls
-        LinkedIn::getAccessToken();
-    } else {
-        // CSRF attack? Or did you mix up your states?
-        exit;
-    }
-} else {
-    if ((empty($_SESSION['expires_at'])) || (time() > $_SESSION['expires_at'])) {
-        // Token has expired, clear the state
-        $_SESSION = array();
-    }
-    if (empty($_SESSION['access_token'])) {
-        // Start authorization process
-        LinkedIn::getAuthorizationCode();
-    }
-}
 
-// Congratulations! You have a valid token. Now fetch your profile
-$user = LinkedIn::fetch('GET', '/v1/people/~');
+$linkedIn = new LinkedIn();
 
+$linkedIn->checkState();
+
+$profileData = $linkedIn->fetch('GET', '/v1/people/~');
 $matches = false;
-if(preg_match('/\bid=([0-9]+)\b/', $user, $matches)) {
+if (preg_match('/\bid=([0-9]+)\b/', $profileData, $matches)) {
     $id = $matches[1];
-    FileWriter::writeProfile($user, $id, FileWriter::$PROFILE_TYPE_LINKEDIN);
+    FileWriter::writeProfile($profileData, $id, FileWriter::$PROFILE_TYPE_LINKEDIN);
 }
 $isLoggedIn = true;
-$profileData = $user;
 include("template.php");
-
-exit;
 
