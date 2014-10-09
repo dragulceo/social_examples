de439cb1fd9143030692d7021d2d8c2fdf59c636
Tavi <dragulceo@gmail.com>
2014-10-09 03:21:00 +0300

    Add TEAMVIEWER constants to config

--- src/Config.php
+++ src/Config.php
@@ -6,7 +6,8 @@ class Config
     public static $FACEBOOK_APP_ID = '';
     public static $FACEBOOK_APP_SECRET = '';
 
-    public static function getFacebookRedirectURL() {
+    public static function getFacebookRedirectURL()
+    {
         return 'http://' . $_SERVER['HTTP_HOST'] . '/facebook.php';
     }
 
@@ -15,8 +16,17 @@ class Config
     public static $LINKEDIN_SCOPE = 'r_fullprofile r_emailaddress';
 
 
-    public static function getLinkedInRedirectURL() {
+    public static function getLinkedInRedirectURL()
+    {
         return 'http://' . $_SERVER['HTTP_HOST'] . '/linkedin.php';
     }
+
+    public static $TEAMVIEWER_APP_ID = '';
+    public static $TEAMVIEWER_APP_SECRET = '';
+    public static function getTeamViewerRedirectURL()
+    {
+        return 'http://' . $_SERVER['HTTP_HOST'] . '/teamviewer';
+    }
+
 }
 
